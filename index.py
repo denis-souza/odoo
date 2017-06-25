57afcede923da1516c8dafdb707b8186ae238438
Denis Souza <denis@merchannew.com.br>
2017-06-24 22:30:59 -0300

    Create class to execute activities odoo.

--- index.py
+++ index.py
@@ -1,34 +1,46 @@
 # -*- coding: utf-8 -*-
-
 import xmlrpclib
 
-url_object = 'http://chocotech.trustcode.com.br/xmlrpc/object'
-url_auth = 'http://chocotech.trustcode.com.br/xmlrpc/common'
-db = 'chocotech'
-username = 'demo'
-password = 'demo'
+#odooActivities.py
+class odooActivities(object):
+	
+  def __init__(self):
+    self.url_object = 'http://chocotech.trustcode.com.br/xmlrpc/object'
+    self.url_auth = 'http://chocotech.trustcode.com.br/xmlrpc/common'
+    self.db = 'chocotech'
+    self.username = 'demo'
+    self.password = 'demo'
+    self.sock = xmlrpclib.ServerProxy(self.url_object)
+    self.uid = 0
 
-# OpenERP Common login Service proxy object 
-sock_common = xmlrpclib.ServerProxy(url_auth)
-uid = sock_common.authenticate(db, username, password, {})
+  def authenticate(self):
+		try:
+			sock_common = xmlrpclib.ServerProxy(self.url_auth)
+			uid = sock_common.authenticate(self.db, self.username, self.password, {})
 
-if uid:
-	print "\nLogin realizado com secesso. \n"
-else:
-	print "\nVerifique as credenciais de acesso."
+			if uid:
+				self.uid = uid
 
-sock = xmlrpclib.ServerProxy(url_object)
+				print "\nLogin realizado com secesso. \n"
+			else:
+				print "\nVerifique as credenciais de acesso."
+		except:
+			print "\nOcorreu um erro, contate com o administrador.\n"
 
-# Insert a new customer
-id_new_customer = sock.execute_kw(db, uid, password, 'res.partner', 'create', [{
-  'name': "Cliente 82",
-  'email': "cliente82@teste.com",
-  'phone': "(48) 99884-9093",
-  'zip': "88037-908"
-}])
-
-if id_new_customer:
-	print 'Cliente cadastrado com sucesso! \n'
-	print 'ID: ' + str(id_new_customer) + '\n'
-else:
-	print 'Não foi possivel salvar o registro do cliente! \n'
+  def insert(self, name, email, phone, zip_code):
+    try:
+      # Insert a new customer.
+      id_new_customer = self.sock.execute_kw(self.db, self.uid, self.password, 'res.partner', 'create', [{
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'zip': zip_code
+      }])
+  
+      if id_new_customer:
+        print '\nCliente cadastrado com sucesso! \n'
+        print 'ID: ' + str(id_new_customer) + '\n'
+      else:
+        print 'Não foi possivel salvar o registro do cliente! \n'
+    except:
+      print "\nOcorreu um erro, contate o administrador.\n"
